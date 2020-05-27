a23a527ad9034679996efa94181cbf067c622970
dev-rb <43100342+dev-rb@users.noreply.github.com>
2020-05-26 20:30:38 -0400

    Quiz

--- src/components/CardsDisplay/index.tsx
+++ src/components/CardsDisplay/index.tsx
@@ -1,8 +1,18 @@
 import * as React from 'react';
+import { Grid, GridList, GridListTile } from '@material-ui/core';
+import { data } from '../../constants/data';
 
 const CardsDisplay: React.FC<{}> = () => {
     return (
+        <GridList cols={5}>
+            {data.map((data) =>
 
+                <GridListTile>
+
+                </GridListTile>
+
+            )}
+        </GridList>
     );
 }
 
