17cf53f9fd2650d474338e55fcc28c27a48bb6ec
dev-rb <rbatra486@gmail.com>
2020-05-27 00:17:23 -0400

    Updates

---
+++ precache-manifest.6cb45c525429a6514ea18e27667d12b4.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "475ee217fc623f06b6932340a4c87f99",
+    "url": "/babies-quiz/index.html"
+  },
+  {
+    "revision": "fbdc7350b2b966590733",
+    "url": "/babies-quiz/static/css/main.a8fb9f9a.chunk.css"
+  },
+  {
+    "revision": "c0bc7e6e7c44b3186596",
+    "url": "/babies-quiz/static/js/2.80353110.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/babies-quiz/static/js/2.80353110.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fbdc7350b2b966590733",
+    "url": "/babies-quiz/static/js/main.a813d193.chunk.js"
+  },
+  {
+    "revision": "15e35cc9451814be3094",
+    "url": "/babies-quiz/static/js/runtime-main.2e990c13.js"
+  },
+  {
+    "revision": "4623be4efacf9fab5611511546fcd9aa",
+    "url": "/babies-quiz/static/media/alligator.4623be4e.png"
+  },
+  {
+    "revision": "6bff8ff75c1e54668e505b3b94544115",
+    "url": "/babies-quiz/static/media/bear.6bff8ff7.png"
+  },
+  {
+    "revision": "12b213907deeb08942148b9a9aae305f",
+    "url": "/babies-quiz/static/media/bee.12b21390.png"
+  },
+  {
+    "revision": "48602853f2536145ccb2501db4683e71",
+    "url": "/babies-quiz/static/media/butterfly.48602853.png"
+  },
+  {
+    "revision": "a58aba1b670685c63918bf9e69fb05d3",
+    "url": "/babies-quiz/static/media/cat.a58aba1b.png"
+  },
+  {
+    "revision": "c4e841587810b1cb3454cb5b88f8b39f",
+    "url": "/babies-quiz/static/media/dog.c4e84158.png"
+  },
+  {
+    "revision": "de153882592bb888cfa1927497680681",
+    "url": "/babies-quiz/static/media/elephant.de153882.png"
+  },
+  {
+    "revision": "5d2fe08f607d4643b4bf935333267c84",
+    "url": "/babies-quiz/static/media/giraffe.5d2fe08f.png"
+  },
+  {
+    "revision": "2e8ef9c8d75d6180f973b4539481f181",
+    "url": "/babies-quiz/static/media/monkey.2e8ef9c8.png"
+  },
+  {
+    "revision": "0f0fca286e8fe1cae1390ad1a779e53a",
+    "url": "/babies-quiz/static/media/penguin.0f0fca28.png"
+  },
+  {
+    "revision": "31854cfb8e889ecf5448ceed1d13fc03",
+    "url": "/babies-quiz/static/media/rabbit.31854cfb.png"
+  },
+  {
+    "revision": "0f84ad4cc02a02d917e0661a5fd77c6d",
+    "url": "/babies-quiz/static/media/snake.0f84ad4c.png"
+  },
+  {
+    "revision": "efa2204e65a0400dbbdb0b1862ff13f0",
+    "url": "/babies-quiz/static/media/tiger2.efa2204e.png"
+  },
+  {
+    "revision": "14f8a5933f7ed29b7f819b5402db0d89",
+    "url": "/babies-quiz/static/media/whale.14f8a593.png"
+  }
+]);
